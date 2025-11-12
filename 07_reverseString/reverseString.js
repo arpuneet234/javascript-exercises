602339dd35e27f50ecc69ddf478c6f934a344adc
Puneet Arora <arorapuneet234@gmail.com>
2025-11-12 18:46:23 +0530

    completed 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let string="";
+    for(let i=word.length-1;i>=0;i--){
+
+        string+=word[i];
+
+    }
+
+    return string;
+
+
 
 };
 
