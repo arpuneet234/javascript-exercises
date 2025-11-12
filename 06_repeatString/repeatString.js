ab8a7ade6c9d1120871c10b09c2c626b4fc6470d
Puneet Arora <arorapuneet234@gmail.com>
2025-11-12 18:23:14 +0530

    completed 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str,n) {
+    let repeatedString="";
+    if(n<0)return "ERROR"
+     
+    for(let i=0;i<n;i++){
+        repeatedString+=str;
+    }
+    return repeatedString;
 
 };
 
