71ae098639511667535812d0fcaef8d103ec9c17
Puneet Arora <arorapuneet234@gmail.com>
2025-11-12 19:25:48 +0530

    completed 07

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let arr=args[0];
+    
+    let k=0;
+
+    let mySet=new Set(); 
+    for(let i=1;i<args.length;i++){
+        mySet.add(args[i]);
+    }
+
+    for(let i=0;i<arr.length;i++){
+        if(!mySet.has(arr[i]))arr[k++]=arr[i];  
+       
+    }
+    arr.length=k;
+
+    return arr;
+    
 };
 
 // Do not edit below this line
