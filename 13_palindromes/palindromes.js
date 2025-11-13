08c51a49a88439b6790b88af332e036de67964bd
Puneet Arora <arorapuneet234@gmail.com>
2025-11-13 14:33:13 +0530

    completed 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+   let string_array=[];
+   string=string.toLowerCase();
+   for(let i=0;i<string.length;i++){
+    if((string[i]>='a'&&string[i]<='z')||(string[i]>='A'&&string[i]<='Z')||(string[i]>='1'&&string[i]<='9')){
+        string_array.push(string[i]);
+    }
+   }
+   let n=string_array.length;
+   let i=0; j=n-1;
+   while(i<=j){
+    if(string_array[i]!=string_array[j])return false;
+    i++;j--;
+   }
+   return true;
+
 
 };
 
