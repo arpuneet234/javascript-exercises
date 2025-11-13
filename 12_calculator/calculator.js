c8abe578b0a91a79705b6e4573dfc433a96062dd
Puneet Arora <arorapuneet234@gmail.com>
2025-11-13 13:45:32 +0530

    completed 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,24 +1,51 @@
-const add = function() {
+const add = function(num1,num2) {
+  return num1+num2;
+
 	
 };
 
-const subtract = function() {
+const subtract = function(num1,num2) {
+  return num1-num2;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  let sum=0;
+  for(let i=0;i<arr.length;i++){
+    sum+=arr[i];
+  }
+
+  return sum;
 	
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let product=1;
+  for(let i=0;i<arr.length;i++){
+    product*=arr[i];
+  }
+  return product;
 
 };
 
-const power = function() {
+const power = function(a,b) {
+  return a**b;
+
+  
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if(num==0)return 1;
+  let fact=1;
+  for(let i=1;i<=num;i++){
+    fact*=i;
+  }
+
+  return fact;
+
+
+
 	
 };
 
