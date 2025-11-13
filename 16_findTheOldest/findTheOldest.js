522767d29d143e4b519e67ae46fd236d9cb5aba1
Puneet Arora <arorapuneet234@gmail.com>
2025-11-13 15:51:34 +0530

    completed 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,38 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let max_pointer=0;
+    let max_age=-1;
+
+    for(let i=0;i<arr.length;i++){
+        let birth=arr[i].yearOfBirth;
+        let death;
+        if(arr[i].yearOfDeath==undefined){
+            let currentDate=new Date();
+            let currentYear=currentDate.getFullYear();
+            death=currentYear;
+        }
+        else death=arr[i].yearOfDeath;
+
+        age=death-birth;
+        if(age>max_age){
+            max_age=age;
+            max_pointer=i;
+        }
+
+
+        
+
+
+
+
+    }
+
+
+    return arr[max_pointer];
+
+
+
+
+
 
 };
 
