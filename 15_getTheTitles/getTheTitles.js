95cf6b010c8af317185a81c951875e36af92a001
Puneet Arora <arorapuneet234@gmail.com>
2025-11-13 15:32:09 +0530

    completed 15

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,4 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(obj) {
+    let array_titles=[];
+    for(let i=0;i<obj.length;i++){
+        array_titles.push(obj[i].title);
+    }
+    return array_titles;
 
 };
 
