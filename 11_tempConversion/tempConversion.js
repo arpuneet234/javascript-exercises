2485d65c374f59a742f033b98e121316e29bfb56
Puneet Arora <arorapuneet234@gmail.com>
2025-11-13 13:28:53 +0530

    completed 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp_celsius=Math.round((temp-32)*(5/9)*10)/10;
+  return temp_celsius;
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp_fahrenheit=Math.round((temp*(9/5)+32)*10)/10;
+  return temp_fahrenheit;
 };
 
 // Do not edit below this line
