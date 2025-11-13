93fb98ff8ddbe9dbc478a30be8810022f340c6bc
Puneet Arora <arorapuneet234@gmail.com>
2025-11-13 14:54:08 +0530

    completed 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num=parseInt(num);
+
+    if(num<0)return "OOPS";
+
+    let first=1;
+    let second=1;
+    if(num==1||num==2)return 1;
+    let result =0;
+    for(i=0;i<num-2;i++){
+        result=first+second;
+        first=second;
+        second=result;
+        
+    }
+
+    return result;
 
 };
 
